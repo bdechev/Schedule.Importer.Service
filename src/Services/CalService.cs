7b3f7e3642ab58fe56093f34a6dba8ecf16792e1
bdechev <borislav.dechev@fourth.com>
2019-02-23 19:15:22 +0200

    Fixed CalService

--- src/Services/CalService.cs
+++ src/Services/CalService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Dtos.Schedule;
 using Ical.Net;
@@ -22,35 +23,84 @@ namespace Services
 
         public async void CreateAppointments(Agenda agenda)
         {
-            var calendar = new Calendar();
+            var calendar = new Calendar()
+            {
+                Name = "VCALENDAR",
+                ProductId = "//Fourth//ScheduleImporter ical.net 4.0//EN",
+                Scale = "GREGORIAN"
+            };
+
             foreach (var scheduledTime in agenda.Schedule)
             {
+                var engineer = agenda.Engineers.FirstOrDefault(x => x.Nickname == scheduledTime.EngineerNickname);
+
                 calendar.Events.Add(new CalendarEvent
                 {
                     Uid = Guid.NewGuid().ToString(),
-                    Name = "OOH_Duty_Hours",
+                    Name = "VEVENT",
                     Class = "PUBLIC",
-                    Summary = "some summary",
-                    Description = "some description",
+                    Summary = $"{engineer.Name} - OOH Appointment",
+                    Description = "API & Services - WFM / Our of office hours support appointment",
                     Created = new CalDateTime(DateTime.UtcNow),
                     Start = new CalDateTime(scheduledTime.From),
                     End = new CalDateTime(scheduledTime.To),
                     Sequence = 0,
-                    Location = "some location"
+                    Status = "CONFIRMED",
                 });
             }
+
             var serializer = new CalendarSerializer(new SerializationContext());
             var serializedCalendar = serializer.SerializeToString(calendar);
             var bytesCalendar = Encoding.UTF8.GetBytes(serializedCalendar);
             var contentStream = new MemoryStream(bytesCalendar);
 
             var calendarsPath = $"{hostingEnvironment.ContentRootPath}/../../calendars/";
-            using (Stream stream = new FileStream($"{calendarsPath}/{calendar.Name}_{DateTime.Now.ToShortDateString()}", FileMode.Create, FileAccess.Write, FileShare.None, (int)contentStream.Length, true))
+            using (Stream stream = new FileStream($"{calendarsPath}/Full_OOH_Schedule_{DateTime.Now.ToString("MM/dd/yyyy").Replace("/", string.Empty)}.ics", FileMode.Create, FileAccess.Write, FileShare.None, (int)contentStream.Length, true))
             {
                 await contentStream.CopyToAsync(stream);
             }
 
             contentStream.Close();
+
+            foreach (var engineer in agenda.Engineers)
+            {
+                var engineerCalendar = new Calendar()
+                {
+                    Name = "VCALENDAR",
+                    ProductId = "//Fourth//ScheduleImporter ical.net 4.0//EN",
+                    Scale = "GREGORIAN"
+                };
+
+                foreach (var scheduledTime in agenda.Schedule.Where(x => x.EngineerNickname == engineer.Nickname))
+                {
+                    engineerCalendar.Events.Add(new CalendarEvent
+                    {
+                        Uid = Guid.NewGuid().ToString(),
+                        Name = "VEVENT",
+                        Class = "PUBLIC",
+                        Summary = $"{engineer.Name} - OOH Appointment",
+                        Description = "API & Services - WFM / Our of office hours support appointment",
+                        Created = new CalDateTime(DateTime.UtcNow),
+                        Start = new CalDateTime(scheduledTime.From),
+                        End = new CalDateTime(scheduledTime.To),
+                        Sequence = 0,
+                        Status = "CONFIRMED",
+                    });
+                }
+
+                var engineerSerializer = new CalendarSerializer(new SerializationContext());
+                var engineerSerializedCalendar = engineerSerializer.SerializeToString(engineerCalendar);
+                var engineerBytesCalendar = Encoding.UTF8.GetBytes(engineerSerializedCalendar);
+                var engineerContentStream = new MemoryStream(engineerBytesCalendar);
+
+                var engineerCalendarsPath = $"{hostingEnvironment.ContentRootPath}/../../calendars/";
+                using (Stream stream = new FileStream($"{engineerCalendarsPath}/{engineer.Name}_OOH_Schedule__{DateTime.Now.ToString("MM/dd/yyyy").Replace("/", string.Empty)}.ics", FileMode.Create, FileAccess.Write, FileShare.None, (int)engineerContentStream.Length, true))
+                {
+                    await engineerContentStream.CopyToAsync(stream);
+                }
+
+                engineerContentStream.Close();
+            }
         }
     }
 }
